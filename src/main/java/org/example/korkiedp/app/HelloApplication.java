8b8e54fbbce1155309bceaffebdf5ebda477bd01
munio7 <maciej.wrotniak03@gmail.com>
2025-05-24 23:35:51 +0200

    Basic changes in HelloApplication regarding title and window size

--- src/main/java/org/example/korkiedp/app/HelloApplication.java
+++ src/main/java/org/example/korkiedp/app/HelloApplication.java
@@ -11,9 +11,11 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/org/example/korkiedp/signup.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/welcome.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        scene.getStylesheets().add(getClass().getResource("/style.css").toExternalForm());
+        stage.setTitle("Korki Manager");
+        stage.setMaximized(true);
         stage.setScene(scene);
         stage.show();
     }
