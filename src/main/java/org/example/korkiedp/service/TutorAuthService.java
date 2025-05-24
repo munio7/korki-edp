2991eba0fb5617ddb354b53b673270f41ba5a10e
munio7 <maciej.wrotniak03@gmail.com>
2025-05-24 23:36:41 +0200

    Added remember me functionality in TutorAuthService

--- src/main/java/org/example/korkiedp/service/TutorAuthService.java
+++ src/main/java/org/example/korkiedp/service/TutorAuthService.java
@@ -1,25 +1,38 @@
 package org.example.korkiedp.service;
 
+import javafx.scene.control.CheckBox;
 import org.example.korkiedp.dao.TutorDAO;
 import org.example.korkiedp.model.Tutor;
+import org.example.korkiedp.session.CurrentSession;
+import org.example.korkiedp.util.ConfigProvider;
 import org.example.korkiedp.util.PasswordUtil;
 
 public class TutorAuthService {
 
-    public Tutor login(String username, String password) {
+    public static Tutor login(String username, String password, boolean rememberMe) {
         Tutor tutor = TutorDAO.findByLogin(username);
         if (tutor == null) {
-            return null;
+            return tutor;
         }
 
-        if(PasswordUtil.check(password,tutor.getPasswordHash()))
+        if(PasswordUtil.check(password,tutor.getPasswordHash())) {
+            CurrentSession.setTutor(tutor);
+            if (rememberMe) {
+                ConfigProvider.set("remember.login",username);
+                ConfigProvider.set("remember.password",password);
+            }
             return tutor;
+        }
         else
-            return null;
+            return tutor;
     }
-    public void logout(Tutor tutor) {}
+    public static void logout() {
+        CurrentSession.clear();
+        ConfigProvider.clear("remember.login");
+        ConfigProvider.clear("remember.password");
+    }
 
-    public Tutor register(Tutor tutor) {
+    public static Tutor register(Tutor tutor) {
         TutorDAO.save(tutor);
         return tutor;
     }
