300a52d827415b750c03f288217cc5a5eb8af0d3
munio7 <maciej.wrotniak03@gmail.com>
2025-05-24 23:35:15 +0200

    Added studentsTableComponent

---
+++ src/main/java/org/example/korkiedp/component/StudentsTableController.java
@@ -0,0 +1,45 @@
+package org.example.korkiedp.component;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import org.example.korkiedp.dao.TutorStudentDAO;
+import org.example.korkiedp.model.Student;
+import org.example.korkiedp.model.TutorStudent;
+import org.example.korkiedp.session.CurrentSession;
+
+import java.util.List;
+
+public class StudentsTableController {
+
+    @FXML
+    private TableView<TutorStudent> studentsTable;
+
+    @FXML
+    private TableColumn<TutorStudent, String> nameColumn;
+    @FXML
+    private TableColumn<TutorStudent, String> levelColumn;
+    @FXML
+    public TableColumn<TutorStudent, String> priceColumn;
+    @FXML
+    public TableColumn<TutorStudent, String> activeColumn;
+
+    @FXML
+    public void initialize() {
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("default_name"));
+        levelColumn.setCellValueFactory(cellData -> {
+            String level = cellData.getValue().getLevel();
+            return new SimpleStringProperty( (level == "" || level == null) ? "Brak" : level );
+        });
+        priceColumn.setCellValueFactory(new PropertyValueFactory<>("default_price"));
+        activeColumn.setCellValueFactory(cellData -> {
+            boolean isActive = cellData.getValue().getActive();
+            return new SimpleStringProperty(isActive ? "Aktywna" : "Nieaktywna");
+        });
+
+        List<TutorStudent> tutorStudents = TutorStudentDAO.findByTutorId(CurrentSession.getTutorId());
+        studentsTable.getItems().setAll(tutorStudents);
+    }
+}
